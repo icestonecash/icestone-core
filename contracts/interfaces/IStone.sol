1e3c0e80591b3fa6dbf092023dff10ac808018e1
Sergey <dev.zaycev@gmail.com>
2022-04-08 20:10:46 +0300

    interfaces/IStone: add additional view functions

--- contracts/interfaces/IStone.sol
+++ contracts/interfaces/IStone.sol
@@ -1,16 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+import "../common/StoneData.sol";
 
 interface IStone {
     function mint(address to, uint256 unlockTime) external;
     function burn(uint256 tokenId) external;
     function withdraw(address to) external returns (uint256);
-    function token() external returns (address);
-    function reserve() external returns (uint256);
+    function token() external view returns (address);
+    function reserve() external view returns (uint256);
     function flash(
         address borrower, 
         uint256 amount, 
         bytes calldata data
     ) external returns (bool);
+    function getApproved(uint256) external view returns (address);
+    function isApprovedForAll(address,address) external view returns (bool);
+    function ownerOf(uint256) external view returns (address);
+    function stonesInfo(uint256) external view returns (StoneData memory);
 }
