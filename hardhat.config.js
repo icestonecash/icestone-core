40453e77ca7642e6edcedc5fa03e362d0bd4bbae
Sergey <dev.zaycev@gmail.com>
2022-04-08 20:11:11 +0300

    hardhat: add bsctest network

--- hardhat.config.js
+++ hardhat.config.js
@@ -24,6 +24,10 @@ module.exports = {
     polygon: {
       url: `https://polygon-rpc.com/`,
       accounts: [`${process.env.PRIVATE_KEY}`]
+    },
+    bsctest: {
+      url: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
+      accounts: [`${process.env.PRIVATE_KEY}`]
     }
   },
   etherscan: {
